d8b80dc07353fd399999d5182c46974913ed60f7
AntoMillanBArcelo <93658398+AntoMillanBArcelo@users.noreply.github.com>
2022-10-13 19:57:32 +0200

    Add files via upload

--- Acciones/Edita.php
+++ Acciones/Edita.php
@@ -5,28 +5,42 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Editar</title>
+    <link rel="stylesheet" href="css/editaCSS.css">
 </head>
 <body>
     <?php
-        $dwes = new PDO('mysql:host=localhost;dbname=prueba', 'root', '');
-    
+        
+
     ?>
+
     <table>
         <form action="" method="post">
             <tr>
                 <td><label for="">ID</label></td>
                 <td><input type="text" id="id" disabled></td>
+                <?php
+        
+
+                ?>
             </tr>
             <tr>
                 <td><label for="">Nombre</label></td>
                 <td><input type="text" id="nombre"></td>
+                <?php
+        
+
+                ?>
             </tr>
             <tr>
-                <td><label for="">Foto</label></td>
-                <td><input type="text" id="foto"></td>
+                <td><label for="">Seleccionar Foto</label></td>
+                <input name="foto" type="file"/>
+                <?php
+        
+
+                ?>
             </tr>
             <tr>
-                <td><input type="submit" value="Guardar" name="btn" /></td>
+                <td><input type="submit" value="Guardar" name="btn"/></td>
             </tr>
         </form>
     </table>
--- Acciones/Nuevo.php
+++ Acciones/Nuevo.php
@@ -1,33 +1,57 @@
 <!DOCTYPE html>
 <html lang="es">
 <head>
-    <meta charset="UTF-8">
+    <meta content="text/html; charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Nuevo</title>
+    <title>Crear</title>
+    <link rel="stylesheet" href="css/nuevoCSS.css">
 </head>
 <body>
-<?php
     
+    <?php    
+        
+        if (isset($_POST['btn'])) 
+        {
+            if (!empty($_POST['id']) && !empty($_POST['nombre']) && !empty($_POST['foto'])) 
+            {
+            $id = $_POST['id'];
+            $nombre = $_POST['nombre'];
+            $fotoMalFormato = $_POST['foto'];
+            $foto = null;
+           // base64_encode($fotoMalFormato);
+
+            $conex = conectabd();
+                insert($conex, $id, $nombre, $foto);
+            //metodoInsert($id, $nombre, $foto)
+            }
+    }
     ?>
+    
     <table>
-        <form action="" method="post">
+        <form action="<?php echo $_SERVER['PHP_SELF'];?>" enctype="multipart/form-data" method="post">
             <tr>
                 <td><label for="">ID</label></td>
-                <td><input type="text" disabled></td>
+                <td><input type="text" id="id"></td>
             </tr>
             <tr>
                 <td><label for="">Nombre</label></td>
-                <td><input type="text"></td>
+                <td><input type="text" id="nombre"></td>
+
             </tr>
             <tr>
-                <td><label for="">Foto</label></td>
-                <td><input type="text"></td>
+            <td>
+                <label for="">Seleccionar Foto</label></td>
+                <input name="foto" type="file"/>    
             </tr>
             <tr>
                 <td><input type="submit" value="Añadir" name="btn" /></td>
             </tr>
         </form>
+        <?php
+        unset($dwes);
+?>
+
     </table>
 </body>
 </html>
